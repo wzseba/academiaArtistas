722878825dc1b514d73f4b63bdf42a06c05256e1
wzseba <wzseba@gmail.com>
2023-10-15 22:53:43 -0300

    update: nuevas pruebas basadas en la refactorizacion

--- src/main/Pruebas.java
+++ src/main/Pruebas.java
@@ -4,20 +4,24 @@ public class Pruebas {
 		
 	public static void main(String[] args) {
 		
-		Academia academia = new Academia(3);
+		Academia academia = new Academia(5);
 		
-        Pintor pintor = new Pintor(123, 2);
-        Escultor escultor = new Escultor(124, 3);
-        Pintor pintorSeba = new Pintor(465, 1);
+		Artista pintorCaleb = new Pintor(545, 1);
+        Artista pintor = new Pintor(123, 2);
+        Artista pintorPepe = new Pintor(823, 2);
+        Artista escultor = new Escultor(124, 3);
+        Artista pintorSeba = new Pintor(465, 1);
         
+        academia.agregarArtista(pintorCaleb);
         academia.agregarArtista(pintor);
         academia.agregarArtista(escultor);
+        academia.agregarArtista(pintorPepe);
         academia.agregarArtista(pintorSeba);
         
         academia.listarPlanillaDeArtistas();
         
         double totalCuotas = academia.getTotalMensualDeCuotas();
-        System.out.println("Total mensual de cuotas: $" + totalCuotas);
+        System.out.println("\nTotal mensual de cuotas: $" + totalCuotas);
 	}
 
 }
