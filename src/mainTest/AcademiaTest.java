238a85dd0cceecf05f3086e444859fb389744d55
wzseba <wzseba@gmail.com>
2023-10-15 22:48:12 -0300

    add: se agrego testing

---
+++ src/mainTest/AcademiaTest.java
@@ -0,0 +1,14 @@
+package mainTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AcademiaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
